6710204b918b165afa58114e3f6a6b733603ab3b
Lautaro Paz Curtet <lauty01@outlook.es>
2025-06-09 16:19:31 -0300

    Agrego Usuario.java
    creo que aca va el metodo compareTo no estoy seguro

--- src/main/java/aed/Usuario.java
+++ src/main/java/aed/Usuario.java
@@ -1,5 +1,35 @@
 package aed;
 
-public class Usuario {
-    int id, monto;
+public class Usuario implements Comparable<Usuario> {
+    private int id;
+    private int saldo;
+
+    public Usuario(int id) {
+        this.id = id;
+        this.saldo = 0;
+    }
+
+    public int obtenerId() {
+        return id;
+    }
+
+    public int obtenerSaldo() {
+        return saldo;
+    }
+
+    public void aumentarSaldo(int monto) {
+        saldo += monto;
+    }
+
+    public void disminuirSaldo(int monto) {
+        saldo -= monto;
+    }
+
+    @Override // esto va aca ???? por las dudas lo agrego
+    public int compareTo(Usuario otro) {
+        if (this.saldo != otro.saldo) {
+            return Integer.compare(this.saldo, otro.saldo);
+        }
+        return Integer.compare(otro.id, this.id); // prioriza el de menor id
+    }
 }
