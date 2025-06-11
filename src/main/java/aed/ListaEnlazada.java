45ef867969e717c78753959f1cb0ceb48dcb3c08
Lautaro Paz Curtet <lauty01@outlook.es>
2025-06-11 18:11:33 -0300

    agrego atributo 'anterior'

--- src/main/java/aed/ListaEnlazada.java
+++ src/main/java/aed/ListaEnlazada.java
@@ -8,10 +8,13 @@ public class ListaEnlazada<T extends Comparable<T>> implements Iterable<T> {
 
     private class Nodo {
         private Nodo sig;
+        private Nodo ant;
         private T valor;
 
         Nodo(T v) {
             valor = v;
+            sig = null;
+            ant = null;
         }
     }
 
@@ -42,16 +45,31 @@ public class ListaEnlazada<T extends Comparable<T>> implements Iterable<T> {
     }
 
     public Handle agregar(T elem) {
-        Nodo nuevo_nodo = new Nodo (elem);
+        Nodo nuevo_nodo = new Nodo(elem);
 
         if (primero == null) {
             primero = nuevo_nodo;
             ultimo = nuevo_nodo;
         } else {
             ultimo.sig = nuevo_nodo;
+            nuevo_nodo.ant = ultimo;
             ultimo = nuevo_nodo;
         }
         return new Handle(nuevo_nodo);
+    }
+
+    public void eliminar(Handle handle) {
+        Nodo nodo = handle.nodo;
+        if (nodo.ant != null) {
+            nodo.ant.sig = nodo.sig;
+        } else {
+            primero = nodo.sig;
+        }
+        if (nodo.sig != null) {
+            nodo.sig.ant = nodo.ant;
+        } else {
+            ultimo = nodo.ant;
+        }
     }
 
     public T obtenerUltimo() {
@@ -59,24 +77,40 @@ public class ListaEnlazada<T extends Comparable<T>> implements Iterable<T> {
     }
 
     public void eliminar(int i) {
+        if (primero == null) return;
+
         if (i == 0) {
-            primero = primero.sig;
-            if (primero == null) {
+            if (primero == ultimo) {
+                primero = null;
                 ultimo = null;
+            } else {
+                primero = primero.sig;
+                primero.ant = null;
             }
             return;
         }
-        Nodo prev = primero;
+
+        Nodo actual = primero;
         int j = 0;
-        while (j < i - 1 && prev != null && prev.sig != null) {
-            prev = prev.sig;
+        while (j < i && actual != null) {
+            actual = actual.sig;
             j++;
         }
-        if (prev != null && prev.sig != null) {
-            if (prev.sig == ultimo) {
-                ultimo = prev;
+
+        if (actual != null) {
+            if (actual == ultimo) {
+                ultimo = actual.ant;
+                if (ultimo != null) {
+                    ultimo.sig = null;
+                }
+            } else {
+                if (actual.ant != null) {
+                    actual.ant.sig = actual.sig;
+                }
+                if (actual.sig != null) {
+                    actual.sig.ant = actual.ant;
+                }
             }
-            prev.sig = prev.sig.sig;
         }
     }
 
